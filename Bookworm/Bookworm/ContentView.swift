f2510dc79b38f733552f238cf319df3f0b5bde20
Juan Dorado <jfdoradotr@icloud.com>
2024-12-23 11:03:15 -0600

    Query the list of books on the main view

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -2,9 +2,13 @@
 // Copyright © Juan Francisco Dorado Torres. All rights reserved.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+  @Environment(\.modelContext) var modelContext
+  @Query var books: [Book]
+
   var body: some View {
     VStack {
       Image(systemName: "globe")
