acdc89dd23c3191b58658784e8235a67b7b6f1d2
Juan Dorado <jfdoradotr@icloud.com>
2024-12-23 10:56:38 -0600

    Create an empty Add book view

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+  var body: some View {
+    Text("Hello")
+  }
+}
+
+#Preview {
+  AddBookView()
+}
