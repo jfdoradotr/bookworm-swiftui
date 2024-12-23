3aa6b459ac43773d2616a5996dc3caa479320fcc
Juan Dorado <jfdoradotr@icloud.com>
2024-12-23 11:00:25 -0600

    Create the basic form to add a new book

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -6,9 +6,47 @@ import SwiftUI
 
 struct AddBookView: View {
   @Environment(\.modelContext) var modelContext
-  
+
+  @State private var title = ""
+  @State private var author = ""
+  @State private var rating = 3
+  @State private var genre = "Fantasy"
+  @State private var review = ""
+
+  let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+
   var body: some View {
-    Text("Hello")
+    NavigationStack {
+      Form {
+        Section {
+          TextField("Name of book", text: $title)
+          TextField("Author's name", text: $author)
+
+          Picker("Genre", selection: $genre) {
+            ForEach(genres, id: \.self) {
+              Text($0)
+            }
+          }
+        }
+
+        Section("Write a review") {
+          TextEditor(text: $review)
+
+          Picker("Rating", selection: $rating) {
+            ForEach(0..<6) {
+              Text(String($0))
+            }
+          }
+        }
+
+        Section {
+          Button("Save") {
+            // add the book
+          }
+        }
+      }
+      .navigationTitle("Add Book")
+    }
   }
 }
 
